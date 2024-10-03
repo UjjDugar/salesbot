68cd377060e0c6fa89ccaa67fe928c9697c5ccdb
UjjDugar <ujjawaldugar@gmail.com>
2024-10-03 11:44:52 +0100

    made some changes

--- faiss_search.py
+++ faiss_search.py
@@ -2,37 +2,41 @@ import pandas as pd
 import numpy as np
 import faiss
 from sentence_transformers import SentenceTransformer
-
-import nltk
-from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize
-import spacy
 import sys
 
-nltk.download('punkt')
-nltk.download('stopwords')
-
-model = SentenceTransformer('all-MiniLM-L6-v2')
+embedding_model = SentenceTransformer('all-MiniLM-L6-v2')
 
 RETURN_K = 3
 
-def preprocess_query(query): # REMOVED
-    stop_words = set(stopwords.words('english'))
-    word_tokens = word_tokenize(query.lower())
-    filtered_query = [word for word in word_tokens if word.isalpha() and word not in stop_words]
-    return ' '.join(filtered_query)
+# get the model to rewrite the query, or make the query using accumulated context about the product
+# modify funct below to return top 3 ids
 
-def faiss_search(query):
-    query_vector = model.encode([query])
+def faiss_search(query, k=10):
+    query_vector = embedding_model.encode([query])
     index = faiss.read_index('product_data.bin')
-    d, i = index.search(query_vector, k=RETURN_K)
-    i = np.array(i).flatten() # should be product ids if database has encoded ids
-    d = np.array(d).flatten()
-    return i, d
+    d, i = index.search(query_vector, k=k)
+    i, d = np.array(i).flatten(), np.array(d).flatten()
+    ids = set()
+    return_ids = []
+
+    for idx in i:
+        id = idx//10000 # remove trailing 4 identifiers
+        if id not in ids:
+            ids.add(id)
+            return_ids.append(id)
+
+        if len(return_ids) == RETURN_K:
+            break
+
+    if len(return_ids) < RETURN_K:
+        return faiss_search(query, k=k*2)
+    
+    return return_ids
+
 
 if __name__ == '__main__':
-    query = sys.argv[1]
+    query = 'Looking for an action shooter game under 20 pounds.'
+    query = 'third-person action shooter game'
     index = faiss.read_index('product_data.bin')
-    product_ids, scores = faiss_search(query)
+    product_ids = faiss_search(query)
     print(f"Product IDs for query '{query}': {product_ids}")
-    print(f"Similarity scores: {scores}")
